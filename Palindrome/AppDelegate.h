db9519595ea1ecc48768634520cb74b88a9be8ef
007jedgar <007j.edgar@gmail.com>
2017-02-02 11:41:49 -0600

    Initial Commit

---
+++ Palindrome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Palindrome
+//
+//  Created by Jonathan Edgar on 2/2/17.
+//  Copyright © 2017 Jonathan Edgar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Palindrome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Palindrome
+//
+//  Created by Jonathan Edgar on 2/2/17.
+//  Copyright © 2017 Jonathan Edgar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
